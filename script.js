6504e2ba36d5318e6b894d5cb45af4367528357d
Rebecca D <rdolmon08@gmail.com>
2023-06-25 11:32:29 -0500

    added css and js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //https://www.youtube.com/watch?v=SgyLy6BlAcI&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=4
+
+const messageInput = document.getElementById("message-input"); 
+
+messageInput.addEventListener("keydown", function(event){
+    if(event.key == "Enter")
+    getMessage();
+})
+
+function getMessage(){
+    document.getElementById("message-output").innerHTML = messageInput.value; 
+    messageInput.value = ""; 
+
+}
